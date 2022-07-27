7bfcf1135dfd9fd042a9f408989fdde61e168dc4
aylnsvr <105922253+aylnsvr@users.noreply.github.com>
2022-07-28 00:48:09 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int zaman =DateTime.Now.Hour;
+
+if(zaman>=6 && zaman <= 12)
+{
+    Console.WriteLine("iyi günler!");
+}
+else if (zaman>12 && zaman<=18)
+{
+    Console.WriteLine("iyi günler!");
+}
+else
+{
+    Console.WriteLine("iyi geceler!");
+}
+Console.WriteLine(zaman >= 6 && zaman <= 12 ? "iyi günler" : zaman > 12 && zaman <= 18 ? "iyi günler" : "iyi geceler");
